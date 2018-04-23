9a01e448636673a98c1bfcabb63e2a2b72b0bf40
xu3352 <xu3352@gmail.com>
2018-04-23 14:09:56 +0800

    增加 timestamp 类型字段补充

--- resources/test.sql
+++ resources/test.sql
@@ -8,6 +8,8 @@ CREATE TABLE `t_student` (
   `sex` tinyint(1) NOT NULL DEFAULT '0' COMMENT '性别',
   `age` int(3) NOT NULL DEFAULT '0' COMMENT '年龄',
   `hobby` tinyint(1) NOT NULL DEFAULT '0' COMMENT '业余爱好:[1.美食 2.K歌 3.逛街 4.游戏 5.旅游 6.运动]',
+  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更改时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8 COMMENT='学生表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='学生表';
 
